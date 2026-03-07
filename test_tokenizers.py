bce7ad4c2569579e53ba1fee57d429a3bfe9790a
Br0k3nDesign <adam.j.moran@gmail.com>
2026-03-06 19:46:31 -0800

    Added support for anthropic

--- test_tokenizers.py
+++ test_tokenizers.py
@@ -1,25 +1,4 @@
-# s = "This is a test"
-
-# t1 = AutoTokenizer.from_pretrained("Xenova/gpt-4o")
-# t2 = AutoTokenizer.from_pretrained("Xenova/claude-tokenizer")
-
-# print("GPT-4o token count:", len(t1.encode(s)))
-# print("Claude token count:", len(t2.encode(s)))
-
 from iqbot import genai
 
 print(type(genai.client))
 print(genai.client.count_tokens("This is a test"))
-
-
-async def main():
-    system_prompt = "Respond with 'hello <name>' for the provided username'"
-    prompt = "Adam"
-    response = await genai.client.send_prompt(None, system_prompt, prompt)
-    print(response[0:1999])
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
